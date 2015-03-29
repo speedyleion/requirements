ae5fed11c59b0b28a60b9a849f9673b0902dafe9
me <me@me.me>
2015-03-28 22:13:34 -0600

    changed the test into a class with an actual assert

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,16 +1,45 @@
+import unittest
 import vimrunner
 # import pdb
 import os
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 
-# initialize vim server, make sure to tell GVIM not to load the user .gvimrc,
-vim = vimrunner.Server(extra_args=['-n', '-U "NONE"'])
+class TestWindowCreation(unittest.TestCase):
+    """
+    This is the class that tests the basic creation of the requirements window.
+    This doesn't do any validation of jumping back and forth or highlighting and
+    tagging the requirements, etc.
+    """
 
-# start GVIM as server and get a client connected to it
-client = vim.start_gvim()
-client.add_plugin(dirname + '/../', 'plugin/requirements.vim')
-client.edit('any_file')
+    def setUp(self):
+        """
+        This just creates a vim server instance.
+        """
+        # initialize vim server, make sure to tell GVIM not to load the user
+        # .gvimrc,
+        self.vim = vimrunner.Server(extra_args=['-n', '-U "NONE"'])
 
-client.command("RequirementsOpen")
-client.quit()
+        # start GVIM as server and get a client connected to it
+        self.client = self.vim.start_gvim()
+        self.client.add_plugin(dirname + '/../', 'plugin/requirements.vim')
+
+    def tearDown(self):
+        self.client.quit()
+
+    def testOpenWindow(self):
+        """
+        This will test the opening of the window and that it is in the correct
+        location and it obeys the global variables set.
+        """
+        self.client.edit('any_file')
+
+        self.client.command("RequirementsOpen")
+        bufname = self.client.eval('bufname(' + self.client.get_active_buffer()
+                                   + ')')
+
+        self.assertEqual(bufname, '__Requirements__', "Expecting "
+                         "`__Requirements__`; Got %s"
+                         %(bufname))
+
+
